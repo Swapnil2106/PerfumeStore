ecfd84af3d40f68d2f91528ea9cb24060b383e99
Swapnil <swapnillkore@gmail.com>
2026-03-03 11:28:41 +0530

    - Created Models

---
+++ Models/Perfume.cs
@@ -0,0 +1,13 @@
+﻿namespace PerfumeStore.Models
+{
+    public class Perfume
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int PerfumeCategoryId { get; set; }              //Foreign Key for Category
+        public PerfumeCategory PerfumeCategory { get; set; }    //Navigation Property for Category
+        public int PerfumeTypeId { get; set; }                  //Foreign Key for Type
+        public PerfumeType PerfumeType { get; set; }            //Navigation Property for Type
+    }
+}
---
+++ Models/PerfumeCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace PerfumeStore.Models
+{
+    public class PerfumeCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Perfume> Perfumes { get; set; }      //One Category -> Many Perfumes
+    }
+}
